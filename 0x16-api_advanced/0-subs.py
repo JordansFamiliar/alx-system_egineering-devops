0b81235923a85bb883345a3854ae624ecf1b8688
Jordan Williams <129204588+JordansFamiliar@users.noreply.github.com>
2024-02-01 21:11:20 +0200

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,24 +1,33 @@
 #!/usr/bin/python3
 """
-queries the Reddit API and returns the number
+Queries the Reddit API and returns the number
 of subscribers (not active users, total subscribers)
 for a given subreddit. If an invalid subreddit is given,
 the function should return 0.
 """
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """returns number of total subscribers"""
-    url = ("https://api.reddit.com/r/{}/about.json".format(subreddit))
+    """Returns the number of total subscribers for a subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'CustomClient/1.0'}
+    
+    # Perform the GET request without following redirects
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code != 200:
-        return (0)
-    response = response.json()
-    if 'data' in response:
-        return (response.get('data').get('subscribers'))
+        return 0
 
-    else:
-        return (0)
+    try:
+        # Try to parse the JSON response
+        data = response.json()
+
+        # Check if 'data' key is present and has 'subscribers' key
+        if 'data' in data and 'subscribers' in data['data']:
+            return data['data']['subscribers']
+        else:
+            return 0
+
+    except ValueError:
+        # JSON decoding failed
+        return 0
